d42482b32d50d6a99307dd7c8ba87ab0331127d5
Tri Vo <tri.vothanh@azureharveynash.onmicrosoft.com>
2017-10-17 17:37:30 +0700

    update

--- src/App.js
+++ src/App.js
@@ -5,12 +5,37 @@ import AppBar from "material-ui/AppBar";
 import Toolbar from "material-ui/Toolbar";
 import Typography from "material-ui/Typography";
 import Button from "material-ui/Button";
+import Drawer from "material-ui/Drawer";
+import List, { ListItem, ListItemIcon, ListItemText } from 'material-ui/List';
+import BottomNavigation, { BottomNavigationButton } from 'material-ui/BottomNavigation';
+
+import injectTapEventPlugin from 'react-tap-event-plugin';
+
+// Icon
 import IconButton from "material-ui/IconButton";
 import MenuIcon from "material-ui-icons/Menu";
+import RestoreIcon from 'material-ui-icons/Restore';
+import FavoriteIcon from 'material-ui-icons/Favorite';
+import LocationOnIcon from 'material-ui-icons/LocationOn';
 
-const styles = () => ({
+injectTapEventPlugin();
+
+const options = [
+  'None',
+  'Atria',
+  'Callisto',
+  'Dione',
+  'Ganymede',
+  'Hangouts Call',
+  'Luna',
+  'Oberon'
+];
+
+const styles = theme => ({
   root: {
-    width: "100%"
+		width: "100%",
+		height: 'auto',
+		minHeight: '200px'
   },
   flex: {
     flex: 1
@@ -21,17 +46,58 @@ const styles = () => ({
   },
   btnMenu: {
 	color: 'yellow'
-  }
+	},
+	
+  list: {
+    width: 250,
+  },
+  listFull: {
+    width: 'auto',
+  },	
 });
 
+
+// Drowler
+const drawerWidth = 240;
+
 class App extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			anchorEl: null,
+			open: false,
+			left: false,
+			value: 1
+		};		
+	}
+
+  toggleDrawer = (side, open) => () => {
+    this.setState({
+      [side]: open,
+    });
+  };
+
+
 	render() {
 		const {classes} = this.props;
+		const { value } = this.state;
+    const sideList = (
+			<List>
+				{options.map((title, index) => (
+					<ListItem key={index} button component="a" href="#">
+						<ListItemText primary={title} />
+					</ListItem>
+				))}
+			</List>
+					
+		);
+				
 		return (
 			<div className="App">
-				<AppBar>
+				<AppBar title="Hello" iconclassnameright = "muidocs-icon-navigation-expand-more">
 					<Toolbar>
-						<IconButton className={classes.menuButton} color="contrast" aria-label="Menu">
+						<IconButton className={classes.menuButton} color="contrast" aria-label="Menu" onClick={this.toggleDrawer('left', true)}>
 							<MenuIcon className={classes.btnMenu} />
 						</IconButton>
 
@@ -42,6 +108,16 @@ class App extends Component {
 						<Button color="contrast">Login</Button>
 					</Toolbar>
 				</AppBar>
+				
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea similique placeat libero quidem exercitationem accusantium esse molestias ad architecto odit iure, eum inventore, in ex necessitatibus delectus harum voluptates ducimus?
+        <Drawer open={this.state.left}>
+						{sideList}			
+        </Drawer>				
+				<BottomNavigation value= {value} className={classes.root}>
+					<BottomNavigationButton label="Recents" icon={<RestoreIcon />} />
+					<BottomNavigationButton label="Favorites" icon={<FavoriteIcon />} />
+					<BottomNavigationButton label="Nearby" icon={<LocationOnIcon />} />
+				</BottomNavigation>				
 			</div>
 		);
 	}
--- src/index.js
+++ src/index.js
@@ -1,8 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { MuiThemeProvider, createMuiTheme } from 'material-ui/styles';
+
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const theme = createMuiTheme({
+    palette: {
+        backgroundColor: 'red'
+    },
+    appBar: {
+        height: 50
+    },
+    overrides: {
+        MuiAppBar: {
+          // Name of the styleSheet
+          root: {
+            // Name of the rule
+            backgroundColor: 'yellow',
+            primary12Color: '#21412a'
+          },
+        },
+      },    
+})
+
+ReactDOM.render(
+    <MuiThemeProvider theme={theme}>
+        <App /> 
+    </MuiThemeProvider>,
+    document.getElementById('root'));
+
+    registerServiceWorker();
